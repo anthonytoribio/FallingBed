5c2006cd5a4914bb1194e63477e3968b77ce37ed
Anthony Toribio <anthonytoribio01@gmail.com>
2023-12-06 21:23:21 -0800

    Updated bedDetection function to get bigger rects

--- bedDetection.py
+++ bedDetection.py
@@ -10,7 +10,7 @@ def getBedBoundingBox() -> tuple:
     brightest_rectangle = None
     totalFrames = 0
     canvas = None
-    while totalFrames < 15:
+    while totalFrames < 20:
         ret, img = cap.read()
         # HSV thresholding to get rid of as much background as possible
         hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
@@ -30,26 +30,27 @@ def getBedBoundingBox() -> tuple:
                                                 cv2.RETR_CCOMP,
                                                 cv2.CHAIN_APPROX_SIMPLE)
 
-        # Filter the rectangle by choosing only the big ones
+        # Filter the rectangle by choosing only the big ones (within a certain area)
         # and choose the brightest rectangle as the bed
+        # also filter with rectangle not starting at the edges of the frame
         max_brightness = 0
         canvas = img.copy()
         for cnt in contours:
             rect = cv2.boundingRect(cnt)
             x, y, w, h = rect
-            if w*h <= 50000 and w*h > 25000:
+            if w*h <= 1000000 and w*h > 25000 and x != 0 and y != 0:
                 mask = np.zeros(img.shape, np.uint8)
                 mask[y:y+h, x:x+w] = img[y:y+h, x:x+w]
                 brightness = np.sum(mask)
-                if brightness > max_brightness:
+                if brightness > max_brightness and (not brightest_rectangle or w*h > brightest_rectangle[2] * brightest_rectangle[3]):
                     brightest_rectangle = rect
+                    #print(brightest_rectangle)
                     max_brightness = brightness
                 # cv2.imshow("mask", mask)
                 # cv2.waitKey(0)
         totalFrames += 1
 
     x, y, w, h = brightest_rectangle
-    print(x,y,w,h)
     cv2.rectangle(canvas, (x,y), (x+w, y+h), (0,255,0), 1)
     cv2.imshow("canvas", canvas)
     cv2.imwrite("samples/bedD.jpg", canvas)
